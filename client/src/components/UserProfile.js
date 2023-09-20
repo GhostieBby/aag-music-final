9a79f69fc23f66010a7754861e023eb2ea68f5fb
Anthony Fort <ajf2195@columbia.edu>
2023-09-20 11:09:56 +0100

    on user profile page, the songs now play from the song list

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -37,17 +37,20 @@ export default function UserProfile() {
         <div>
           <h1>{userProfile.username}</h1>
           <h2>Likes: {userProfile.likes}</h2>
-          {userProfile.userSongs.length > 0 && (
-            <Container>
-              <Row>
-                {userProfile.userSongs.map(song => (
-                  <Col key={song.soundCloudId}>{song.soundCloudId}</Col>
-                ))}
-              </Row>
-            </Container>
-          )}
+          {userProfile.userSongs.length > 0 && 
+            userProfile.userSongs.map(song => (
+              <iframe
+                key={song.soundCloudId} 
+                width="100%"
+                height="100"
+                allow="autoplay"
+                src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.soundCloudId}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`}>
+              </iframe>
+            ))
+          }
         </div>
       ) : null}
     </>
   )
+  
 }
