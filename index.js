9e9aede99f753fca76932e8b4ff88e26a0f1572b
Anthony Fort <ajf2195@columbia.edu>
2023-09-21 10:28:05 +0100

    added serve static files in index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import mongoose from 'mongoose'
 import router from './config/routes.js'
 import 'dotenv/config'
 
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const app = express()
 
 // Parse JSON body
@@ -17,6 +23,14 @@ app.use((req, res, next) => {
 })
 
 app.use('/api', router)
+
+// SERVE STATIC FILES IN PRODUCTION
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
+
 
 // ?  Route Not Found
 app.use((req, res) => {
